42720d6addfaae2c8af4e639554627ab690ad158
mottasimsadi <mottasimsadi@gmail.com>
2025-07-16 03:33:07 +0600

    Project initialized with the necessary files

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const cors = require("cors");
+
+const dotenv = require("dotenv");
+dotenv.config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./admin-key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// middleware/auth.js
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+
+  const idToken = authHeader.split(" ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.firebaseUser = decodedToken; // You can access user info like uid, email, etc.
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Invalid token from catch" });
+  }
+};
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    const foodCollection = client.db("foodDB").collection("foodShare");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+// Root route
+app.get("/", verifyFirebaseToken, async (req, res) => {
+  res.send("Food Share is running perfectly!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Food Share server is running on port ${PORT}`);
+});
